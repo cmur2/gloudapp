b88724cdf078111f67802673c0a266f33a8670b8
cn <chrnicolai@gmail.com>
2011-09-09 16:16:30 +0200

    Patches for cloudapp_api; URL-copy to clipboard

--- main.rb
+++ main.rb
@@ -5,19 +5,61 @@ require 'rubygems'
 require 'gtk2'
 require 'cloudapp_api'
 
-$tmp_dir = "/tmp"
+SCRN_TIME_FMT = '%d%m%y-%H%M%S'
+TMP_DIR = "/tmp"
+
+# patches for cloudapp_api
+module CloudApp
+	class Account
+		attr_reader :subscription_expires_at
+	end
+
+	class Drop
+		def slug
+			url.split(/\//).last
+		end
+
+		def self.find(id)
+			res = get "http://#{$domain}/#{id}"
+			res.ok? ? Drop.new(res) : bad_response(res)
+		end
+
+		attr_reader :source
+	end
+
+	class Multipart
+		def payload
+			{
+				:headers => {
+					"User-Agent"   => "Ruby.CloudApp.API",
+					"Content-Type" => "multipart/form-data; boundary=#{boundary}"},
+				:body => @body
+			}
+		end
+	end
+end
+
 
 @client = CloudApp::Client.new
 
-# all following commands need authentication
-@client.authenticate("me", "there")
+if ARGV.length == 2
+	# assume that's username and password in ARGV
+	@client.authenticate(ARGV[0], ARGV[1])
+else
+	# TODO: launch an Gtk Dialog asking for login
+	puts "You should provide username and password as arguments!"
+	exit 1
+end
+
+# check whether auth was successful
 begin
 	@acc = CloudApp::Account.find
-	#$domain = @acc.domain.nil? ? 'cl.ly' : @acc.domain
+	$domain = @acc.domain.nil? ? 'cl.ly' : @acc.domain
 rescue
 	abort "Auhtentication failed: #{$!.to_s}"
 end
 
+# create status icon
 si = Gtk::StatusIcon.new
 si.stock = Gtk::Stock::DIALOG_INFO
 #si.pixbuf = Gdk::Pixbuf.new('/path/to/image')
@@ -25,17 +67,30 @@ si.tooltip = 'GloudApp'
 
 # left click
 si.signal_connect('activate') do
-	p "Taking a screenshot... (hopefully)"
-	file = File.join($tmp_dir, "bam.png")
-	system("import -window root -resize 500 #{file}")
-	drop = @client.upload file
-	p drop.url
+	file = File.join(TMP_DIR, "Screenshot #{Time.now.strftime(SCRN_TIME_FMT)}.png")
+	
+	puts "Taking screenshot..."
+	
+	# TODO: find rubish way to take screen shots
+	# make screenshot via image magick:
+	system("import -window root \"#{file}\"")
+	
+	puts "Uploading it..."
+	
+	drop = @client.upload(file)
+	
+	puts "URL (in clipboard, too): #{drop.url}"
+	
+	# copy URL to clipboard
+	cb = Gtk::Clipboard.get(Gdk::Selection::CLIPBOARD)
+	cb.text = drop.url
 end
 
 # popup menu
 info = Gtk::ImageMenuItem.new(Gtk::Stock::INFO)
 info.signal_connect('activate') do
 	#p "Embedded: #{si.embedded?}"; p "Visible: #{si.visible?}"; p "Blinking: #{si.blinking?}"
+	# TODO: show information dialog
 end
 
 quit = Gtk::ImageMenuItem.new(Gtk::Stock::QUIT)
