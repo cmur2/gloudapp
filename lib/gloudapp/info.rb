f6477e1782d4e265f3ae1828dfb9204f7261d549
cn <chrnicolai@gmail.com>
2011-09-28 14:40:36 +0200

    Bump gem version

--- lib/gloudapp/info.rb
+++ lib/gloudapp/info.rb
@@ -1,6 +1,6 @@
 module GloudApp
 	module Info
-		VERSION = "1.1.1"
+		VERSION = "1.2.0"
 		AUTHORS = [
 			["Christian Nicolai", "chrnicolai@gmail.com"],
 			["Jan Graichen", "jan.graichen@altimos.de"]
@@ -9,7 +9,7 @@ module GloudApp
 			["Jan Graichen", "jan.graichen@altimos.de"]
 		]
 		HOMEPAGE = "https://github.com/cmur2/gloudapp"
-		SUMMARY = "CloudApp client for GNOME/GTK"
+		SUMMARY = "CloudApp (tm) applet for Gnome/GTK2"
 		COPYRIGHT = "Copyright 2011 GloudApp Team"
 		LICENSE = "Apache License, Version 2.0"
 	end
